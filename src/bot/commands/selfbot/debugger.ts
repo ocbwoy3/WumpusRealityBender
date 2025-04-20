359fac35b133ff875cf89dde42d9270e2648dbb3
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2025-04-20 22:22:11 +0300

    dddddddddddddddddd

--- src/bot/commands/selfbot/debugger.ts
+++ src/bot/commands/selfbot/debugger.ts
@@ -1,9 +1,11 @@
 import { Subcommand } from "@sapphire/plugin-subcommands";
 import {
 	ApplicationIntegrationType,
+	AttachmentBuilder,
 	InteractionContextType,
 	MessageFlags
 } from "discord.js";
+import { RelationshipManager } from "discord.js-selfbot-v13";
 import { getAuthenticatedUser } from "noblox.js";
 import { SelfbotClient } from "selfbot";
 
@@ -20,6 +22,10 @@ export class UserCommand extends Subcommand {
 				{
 					name: "accounts",
 					chatInputRun: "chatInputListAccounts"
+				},
+				{
+					name: "export-friends",
+					chatInputRun: "chatInputExportDiscordFriends"
 				}
 			]
 		});
@@ -47,7 +53,21 @@ export class UserCommand extends Subcommand {
 						)
 						.addBooleanOption((a) =>
 							a
-								.setName("ephemral")
+								.setName("ephemeral")
+								.setDescription(
+									"Determines if the message should be ephemeral. (Default = true)"
+								)
+						)
+				)
+				.addSubcommand((c) =>
+					c
+						.setName("export-friends")
+						.setDescription(
+							"Exports ALL of your discord friends to JSON."
+						)
+						.addBooleanOption((a) =>
+							a
+								.setName("ephemeral")
 								.setDescription(
 									"Determines if the message should be ephemeral. (Default = true)"
 								)
@@ -63,7 +83,7 @@ export class UserCommand extends Subcommand {
 		if (eph === null) eph = true;
 		await interaction.deferReply({
 			withResponse: true,
-			flags: eph ? [ MessageFlags.Ephemeral ] : []
+			flags: eph ? [MessageFlags.Ephemeral] : []
 		});
 		try {
 			const robloxAcc = await getAuthenticatedUser();
@@ -73,6 +93,40 @@ export class UserCommand extends Subcommand {
 						`> **Roblox:** [${robloxAcc.displayName} (@${robloxAcc.name})](<https://fxroblox.com/users/${robloxAcc.id}>)`,
 						`> **Discord:** <@${SelfbotClient.user!.id}>`
 					].join("\n")
+				})
+				.catch((a) => {});
+		} catch (x) {
+			return interaction
+				.followUp({
+					content: `> ${x}`
+				})
+				.catch((a) => {});
+		}
+	}
+
+	public async chatInputExportDiscordFriends(
+		interaction: Subcommand.ChatInputCommandInteraction
+	) {
+		let eph = interaction.options.getBoolean("ephemeral", false);
+		if (eph === null) eph = true;
+		await interaction.deferReply({
+			withResponse: true,
+			flags: eph ? [MessageFlags.Ephemeral] : []
+		});
+		try {
+			const friends = ((await SelfbotClient.relationships.fetch(undefined,{cache:true, force: false})) as RelationshipManager).toJSON().filter(a=>(a.type as any)==="FRIEND").map(a=>({id: a.id, friendsSince: a.since}));
+			return interaction
+				.followUp({
+					files: [
+						new AttachmentBuilder(
+							Buffer.from(
+								JSON.stringify(
+									friends
+								)
+							),
+							{ name: "friends.json" }
+						)
+					]
 				})
 				.catch((a) => {});
 		} catch (x) {
